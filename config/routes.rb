f1fdcbba61fc39cda5937dd73373c9fcc613b913
Fernando Nieto <lestairon@MacBook-Pro-de-Fernando.local>
2019-04-23 12:00:49 -0500

    Make layouts prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get  '/signup',     to: 'users#new'
+  get  '/home',    to: 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/login',   to: 'static_pages#login'
+  get  '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
